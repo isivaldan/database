b9a1014e3604d057246b1eb18226865a4aefdebc
Isidora Valenzuela <isi.valenzuela@gmail.com>
2020-06-25 15:14:13 -0400

    cambio casos

--- casos/caso2.sql
+++ casos/caso2.sql
@@ -35,5 +35,7 @@ CREATE TABLE prueba
 (id  INT PRIMARY KEY,
  puntaje INT,
  rut_alu VARCHAR(15),
- FOREIGN KEY (rut_alu) REFERENCES alumno(rut)
+ rut_prof VARCHAR(15),
+ FOREIGN KEY (rut_alu) REFERENCES alumno(rut),
+ FOREIGN KEY (rut_prof) REFERENCES profesor(rut)
 );
