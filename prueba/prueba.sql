b1632ca4952a7e14b9c20f7ed2e1d0ef67e97e70
Isidora Valenzuela <isi.valenzuela@gmail.com>
2020-06-26 11:00:38 -0400

    prueba

--- prueba/prueba.sql
+++ prueba/prueba.sql
@@ -1,8 +1,5 @@
-DROP TABLE ventas;
-DROP TABLE facturas;
-DROP TABLE productos;
-DROP TABLE clientes ;
-DROP TABLE categorias;
+
+DROP DATABASE ventas;
 --Parte 1: Crear modelo lógico para el siguiente caso
 CREATE DATABASE ventas;
 \c ventas
@@ -24,7 +21,7 @@ CREATE TABLE productos
 (id  INT PRIMARY KEY,
  nombre VARCHAR(20),
  descrip VARCHAR(50),
- valor INT,
+ valor FLOAT,
  id_cat INT,
  FOREIGN KEY (id_cat) REFERENCES categorias(id)
 );
@@ -33,9 +30,9 @@ CREATE TABLE productos
 CREATE TABLE facturas
 (num_fact INT PRIMARY KEY ,
  fecha_fact DATE,
- sub_total INT,
- iva INT,
- total INT,
+ sub_total FLOAT,
+ iva FLOAT,
+ total FLOAT,
  id_cliente INT,
  FOREIGN KEY (id_cliente) REFERENCES clientes(id)
 );
@@ -49,152 +46,128 @@ CREATE TABLE ventas
 );
 
 -- Parte 2: Creando el modelo en la base de datos
--- Se debe crear el modelo en la base de datos, en una base de datos llamada prueba, e insertar lossiguientes registros:
+-- Se debe crear el modelo en la base de datos, en una base de datos llamada prueba, e insertar los siguientes registros:
+
+
 -- 5 clientes
 INSERT INTO clientes (id, rut, nombre, direccion) 
-VALUES( 1,'11111111-1','Daniela','Toesca 267');
+    VALUES  ( 1,    '9512238-8',   'Daniela',  'Toesca 267'),
+            ( 2,    '19814388-k',   'Andres',   'Padre las casas 321'),
+            ( 3,    '17333539-3',   'Carlos',   'Alameda 455'),
+            ( 4,    '19116377-k',   'Trinidad', 'Pajaritos 267'),
+            ( 5,    '18597094-9',   'Mariela',  'Padre Hurtado 267');
 
-INSERT INTO clientes (id, rut, nombre, direccion) 
-VALUES( 2,'11111111-1','Andres','Padre las casas 321');
 
-INSERT INTO clientes (id, rut, nombre, direccion) 
-VALUES( 3,'11111111-1','Carlos','Alameda 455');
 
-INSERT INTO clientes (id, rut, nombre, direccion) 
-VALUES( 4,'11111111-1','Trinidad','Pajaritos 267');
-
-INSERT INTO clientes (id, rut, nombre, direccion) 
-VALUES( 5,'11111111-1','Mariela','Padre Hurtado 267');
 
 -- 3 categorias
 INSERT INTO categorias (id, nombre, descrip) 
-VALUES( 1,'Vestuario','Ropa distinta');
-INSERT INTO categorias (id, nombre, descrip) 
-VALUES( 2,'Electronica','Celulares, notebooks,TV');
-INSERT INTO categorias (id, nombre, descrip) 
-VALUES( 3,'Deportes','Accesorios para deportes');
+    VALUES  ( 1,    'Vestuario',    'Ropa distinta'), 
+            ( 2,    'Electronica',  'Celulares, notebooks, TV'), 
+            ( 3,    'Deportes',     'Accesorios para deportes');
+
+
 
 -- 8 productos
 INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 1,'Camisa','camisa formal',20,1);
+    VALUES  ( 1,    'Camisa',           'camisa formal',        20,     1), 
+            ( 2,    'Pantalon',         'Pantalon formal',      25,     1), 
+            ( 3,    'baston trekking',  'baston de trekking ',  15,     3), 
+            ( 4,    'Pelota',           'Pelota futbol ',       10,     3), 
+            ( 5,    'Celular',          'Celular I Phone',      100,    2), 
+            ( 6,    'TV',               'TV AOC',               150,    2), 
+            ( 7,    'Camara',           'Camara Canon',         130,    2), 
+            ( 8,    'Notebook',         'Notebook HP',          200,    2);
 
-INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 2,'Pantalon','Pantalon formal',25, 1);
-
-INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 3,'baston trekking','baston de trekking ',15, 3);
-
-INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 4,'Pelota','Pelota futbol ',10, 3);
-
-INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 5,'Celular','Celular I Phone',100, 2);
-
-INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 6,'TV','TV AOC',150, 2);
-
-INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 7,'Camara','Camara Canon',130, 2);
-
-INSERT INTO productos (id, nombre, descrip, valor, id_cat) 
-VALUES( 8,'Notebook','Notebook HP',200, 2);
 
 -- 10 facturas
 
--- 2 para el cliente 1, con 2 y 3 productos
+
+
+-- Para el cliente 1: 2 facturas con 2 y 3 productos respectivamente
+INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
+    VALUES  ( 1, '2020-02-02', 45, 0.19, 53.55, 1);
+
+INSERT INTO ventas (id_fact, id_prod, cantidad ) 
+    VALUES  ( 1, 1, 1),
+            ( 1, 2, 1);
 
 INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 2,'2020-02-02',45,0.19, 53.55,1);
+    VALUES  ( 2, '2020-02-18', 135, 0.19, 160.65, 1);
 
 INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 1,1,1);
+    VALUES  ( 2, 3, 1), 
+            ( 2, 4, 1), 
+            ( 2, 5, 1);
+
+
+
+-- Para el cliente 2: 3 facturas con 3, 2 y 3 productos respectivamente
+INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
+    VALUES( 3, '2020-02-18', 50, 0.19, 58, 2); 
+
 INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 1,2,1);
+    VALUES  ( 3, 2, 1), 
+            ( 3, 3, 1), 
+            ( 3, 4, 1);
 
 INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 1,'2020-02-18',135,0.19, 160.65,1);
+    VALUES  ( 4, '2020-02-18', 110, 0.19, 130.9, 2);
 
 INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 2,3,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 2,4,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 2,5,1);
-
--- 3 para el cliente 2, con 3, 2 y 3 productos
-INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 3,'2020-02-18',50,0.19, 58, 2);
-
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 3,2,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 3,3,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 3,4,1);
+    VALUES  ( 4, 5, 1), 
+            ( 4, 6, 1);
 
 INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 4,'2020-02-18',110,0.19, 130.9, 2);
+    VALUES  ( 5,'2020-02-18', 60, 0.19, 62.85, 2);
+
 INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 4,5,1);
+    VALUES  ( 5, 1, 1), 
+            ( 5, 2, 1), 
+            ( 5, 3, 1);
+
+
+
+
+-- para el cliente 3: 1 factura con 1 producto
+INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
+    VALUES  ( 6, '2020-02-18', 200, 0.19, 238, 3);
+
 INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 4,6,1);
+    VALUES  ( 6, 8, 1);
+
+
+
+
+-- para el cliente 4: 4 facturas con 2, 3, 4 y 1 productos respectivamente
+INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
+    VALUES  ( 7, '2020-02-04', 45, 0.19, 53.55, 4);
+INSERT INTO ventas (id_fact, id_prod, cantidad ) 
+    VALUES  ( 7, 1, 1), 
+            ( 7, 2, 1);
 
 INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 5,'2020-02-18',60,0.19, 62.85, 2);
-
+    VALUES  ( 8, '2020-04-18', 60, 0.19, 62.85, 4);
 INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 5,1,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 5,2,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 5,3,1);
-
-
--- 1 para el cliente 3, con 1 producto
-INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 6,'2020-02-18',200,0.19, 238, 3);
-
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 6,8,1);
-
-
--- 4 para el cliente 4, con 2, 3, 4 y 1 producto
-INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 7,'2020-02-04',45,0.19, 53.55, 4);
-
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 7,1,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 7,2,1);
+    VALUES  ( 8, 1, 1), 
+            ( 8, 2, 1),        
+            ( 8, 3, 1);
 
 INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 8,'2020-04-18',60,0.19, 62.85, 4);
-
+    VALUES  ( 9, '2020-05-18', 580, 0.19, 690.2, 4);
 INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 8,1,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 8,2,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 8,3,1);
+    VALUES  ( 9, 5, 1), 
+            ( 9, 6, 1), 
+            ( 9, 7, 1), 
+            ( 9, 8, 1);
 
 INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 9,'2020-05-18',580,0.19, 690.2, 4);
+    VALUES  ( 10, '2020-02-18', 100, 0.19, 119, 4);
+INSERT INTO ventas (id_fact, id_prod, cantidad ) 
+    VALUES  ( 10, 5, 1);
 
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 9,5,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 9,6,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 9,7,1);
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 9,8,1);
-
-INSERT INTO facturas (num_fact, fecha_fact, sub_total, iva, total,id_cliente ) 
-VALUES( 10,'2020-02-18',100,0.19, 119, 4);
-
-INSERT INTO ventas (id_fact, id_prod, cantidad ) 
-VALUES( 10,5,1);
--- Parte 3: ConsultasRealizar las siguientes consultas:
+-- Parte 3: Consultas q
+-- Realizar las siguientes consultas:
 -- ¿Que cliente realizó la compra más cara?
 SELECT nombre FROM clientes
 JOIN facturas ON clientes.id=facturas.id_cliente 
@@ -206,7 +179,7 @@ JOIN facturas ON clientes.id=facturas.id_cliente
 WHERE facturas.total>100;
 
 -- ¿Cuantos clientes han comprado el producto 6.
-SELECT DISTINCT nombre FROM clientes
+SELECT DISTINCT count(nombre) FROM clientes
 JOIN facturas ON clientes.id=facturas.id_cliente 
 JOIN ventas ON facturas.num_fact=ventas.id_fact 
 WHERE ventas.id_prod=6;
